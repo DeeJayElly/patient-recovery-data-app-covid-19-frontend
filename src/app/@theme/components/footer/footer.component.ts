45482a20ed4bd3afea6e61f4cee2b5ad84a93704
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2020-04-05 04:57:53 -0600

    Fixes for footer & header

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -1,18 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">
-      Created with ♥ by <b><a href="https://akveo.page.link/8V2f" target="_blank">Akveo</a></b> 2019
+      <span class="created-by">
+      Copyright © 2020 <b><a href="https://www.datavscorona.com/" target="_blank">
+        Data vs Corona Hackaton - Patient Recovery Data Application - Covid-19</a></b>
     </span>
-    <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
-    </div>
+      <div class="socials">
+          <a href="https://github.com/DeeJayElly/patient-recovery-data-app-covid-19-frontend/"
+             target="_blank" class="ion ion-social-github"></a>
+      </div>
   `,
 })
 export class FooterComponent {
