abe93e87d12488ce246f551c59a41a917e74c9e6
Aleksandar Djordjevic <aca_eliminator@hotmail.com>
2020-04-05 05:27:15 -0600

    Fixes for build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  apiUrl: 'http://77010085.ngrok.io/api',
+  apiUrl: 'https://77010085.ngrok.io/api',
   production: false,
 };
