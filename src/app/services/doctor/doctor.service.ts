bbed7ffaac58e05fe8640d1f9b0a3f1af4e8ccf8
nikolllaa <asknikola1996@hotmail.com>
2020-04-05 11:30:30 +0200

    Get all doctors

--- src/app/services/doctor/doctor.service.ts
+++ src/app/services/doctor/doctor.service.ts
@@ -2,13 +2,17 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Doctor} from '../../models/doctor.model';
 import {environment} from '../../../environments/environment';
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class DoctorService {
   constructor(private http: HttpClient) {
   }
 
-  getAll() {
-    return this.http.get<Doctor[]>(`${environment.apiUrl}/doctors`);
+  public getAll() {
+    return this.http.get<Doctor[]>(`${environment.apiUrl}/doctor`)
+      .pipe(map((response) => {
+        return response;
+      }));
   }
 }
